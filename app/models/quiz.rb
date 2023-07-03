5d56aba75b5716d6b92124fad38c42ae45cb3c38
Admin Administrator <admin@Admins-MacBook-Pro.local>
2023-07-03 10:05:46 +0300

    Initial commit

---
+++ app/models/quiz.rb
@@ -0,0 +1,13 @@
+class Quiz < ActiveRecord::Base
+end
+
+# == Schema Information
+#
+# Table name: quizzes
+#
+#  id          :bigint           not null, primary key
+#  description :string
+#  name        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
